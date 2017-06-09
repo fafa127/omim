2c95cc1375f366b7a79d02bfa4e076309bdabe31
fafa127 <fafa.lbo@gmail.com>
2017-06-10 02:59:53 +0430

    Create languages.hpp

--- sound/tts/languages.hpp
+++ sound/tts/languages.hpp
@@ -13,7 +13,7 @@ namespace turns
 {
 namespace sound
 {
-array<pair<string, string>, 28> const kLanguageList =
+array<pair<string, string>, 29> const kLanguageList =
 {{
   {"en", "English"},
   {"ru", "Русский"},
@@ -29,6 +29,7 @@ array<pair<string, string>, 28> const kLanguageList =
   {"cs", "Čeština"},
   {"da", "Dansk"},
   {"el", "Ελληνικά"},
+  {"fa", "فارسی"},
   {"fi", "Suomi"},
   {"hi", "हिंदी"},
   {"hr", "Hrvatski"},
